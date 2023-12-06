75a9a157e231c4bce8cf84bcae9eb2373b7b99d7
LMetal <leogall2002@gmail.com>
2023-12-06 09:16:28 +0100

    new test structure, part3

--- CompilatoreAcDc/src/test/TestToken.java
+++ CompilatoreAcDc/src/test/TestToken.java
@@ -6,13 +6,8 @@ import static token.TokenType.*;
 
 import org.junit.jupiter.api.Test;
 
-import scanner.LexicalException;
-import scanner.Scanner;
 import token.Token;
-import token.TokenType;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 class TestToken {
 
@@ -20,9 +15,15 @@ class TestToken {
 	void testToken() {
 		Token t1 = new Token(INT, 1, "3");
 		assertEquals("<INT,r:1,3>", t1.toString());
+		assertEquals(INT, t1.getTipo());
+		assertEquals(1, t1.getRiga());
+		assertEquals("3", t1.getVal());
 
 		Token t2 = new Token(PRINT, 3);
 		assertEquals("<PRINT,r:3>", t2.toString());
+		assertEquals(PRINT, t2.getTipo());
+		assertEquals(3, t2.getRiga());
+		assertNull(t2.getVal());
 	}
 
 
